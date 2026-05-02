14187da3c5877ca5b5ed4b722ea10f81b7eb6556
daria <m2403815@edu.misis.ru>
2026-05-02 19:49:34 +0300

    fix: change table check ins

--- back/migrations/00004_create_check_ins_table.sql
+++ back/migrations/00004_create_check_ins_table.sql
@@ -2,9 +2,8 @@
 CREATE TABLE check_ins (
     id SERIAL PRIMARY KEY,
     habit_id INT NOT NULL REFERENCES habits(id) ON DELETE CASCADE,
-    completed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    completed_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    CONSTRAINT unique_daily_checkin UNIQUE (habit_id, completed_date)
+    completed_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    CONSTRAINT unique_daily_checkin UNIQUE (habit_id, completed_at)
 );
 
 -- +goose Down
