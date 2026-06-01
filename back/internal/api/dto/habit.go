5fd37d00ee7a58f665cfaffdc99bb3abbef27121
daria <m2403815@edu.misis.ru>
2026-06-01 18:45:50 +0300

    fix: modify some files

--- back/internal/api/dto/habit.go
+++ back/internal/api/dto/habit.go
@@ -11,7 +11,7 @@ type CreateHabitRequest struct {
 	Description string   `json:"description"`
 	Frequency   string   `json:"frequency"`
 	TemplateID  *uint    `json:"template_id,omitempty"`
-	Tags        []uint `json:"tags,omitempty"`
+	Tags        []string `json:"tags,omitempty"`
 }
 
 type UpdateHabitRequest struct {
